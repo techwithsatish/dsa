5a005c8d76e223a5fa39bd1644ad506f7489b001
techwithsatish <satish15.dev@gmail.com>
2025-05-07 06:04:37 +0530

    sloved tree-path-finder question

---
+++ 4.BinaryTree-1/56_tree-path-finder.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+
+
+
+
+def path_finder(root, target):
+    result = _path_finder(root, target)
+    if result is None:
+        return None
+    else:
+        return result[::-1]
+
+
+def _path_finder(root, target):
+    if root is None:
+        return None
+
+    if root.val == target:
+        return [root.val]
+
+    left_path = _path_finder(root.left, target)
+    if left_path is not None:
+        left_path.append(root.val)
+        return left_path
+
+    right_path = _path_finder(root.right, target)
+    if right_path is not None:
+        right_path.append(root.val)
+        return right_path
+
+    return None
+
+print(path_finder(a, 'e'))
